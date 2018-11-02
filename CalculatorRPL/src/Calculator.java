7e9b1d68eb2c6ef759f3de66e41d316241163adf
TIMOTHY LAWRENCE <i16064@ftis>
2018-11-02 14:01:52 +0700

    implementasi plus minus

--- CalculatorRPL/src/Calculator.java
+++ CalculatorRPL/src/Calculator.java
@@ -14,12 +14,12 @@ public class Calculator {
     }
     
     public float plus (float a, float b){
-        
+        return a + b;
         
     }
     
     public float minus (float a, float b){
-        
+        return a-b;
         
     }
     
