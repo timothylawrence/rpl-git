e9210439fd29f9e599ffd2f0df4a62c8ca0c0158
TIMOTHY LAWRENCE <i16064@ftis>
2018-11-02 13:37:29 +0700

    calculator initial commit

---
+++ CalculatorRPL/src/Calculator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i16064
+ */
+public class Calculator {
+
+    public Calculator() {
+    }
+    
+    public float plus (float a, float b){
+        
+        
+    }
+    
+    public float minus (float a, float b){
+        
+        
+    }
+    
+    public float multiply (float a, float b){
+        
+        
+    }
+    
+    public float plus (float a, float b){
+        
+        
+    }
+}
